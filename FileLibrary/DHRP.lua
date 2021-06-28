b9a513c5b73875f5186467e09df65b4784aa6b70
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-06-28 02:24:20 +0100

    Update DHRP.lua

--- FileLibrary/DHRP.lua
+++ FileLibrary/DHRP.lua
@@ -1,1 +1,45 @@
+--[[ AUTO AUDIO ENCODE ]]--
 
+local Hex = ("%#x")
+local Met = (getrawmetatable(game))
+local Net = (Met.__namecall)
+local Clo = (newcclosure)
+local Sro = setreadonly
+
+local HexEncode = function(String)
+    
+   local CTH = function(c)
+      return string.format("%%%02X", string.byte(c))
+   end
+   
+   if String == (nil) then return end
+   
+   String = String:gsub("\n", "\r\n")
+   String = String:gsub("([^% ])", CTH)
+   String = String:gsub(" ", "+")
+   
+   return String
+end
+
+Sro(Met, false)
+
+Met.__namecall = Clo(function(dababy, ...)
+    
+    local args = { ... }
+    
+    if args[1] == "PlaySong" then
+       
+       args[2] = string.format(Hex, args[2])
+       args[2] = HexEncode(args[2])
+       
+       print("Successful Encode:", args[2])
+       
+       return Net(dababy, unpack(args))
+    end
+    
+    return Net(dababy, ...)
+end)
+
+Sro(Met, true);
+
+--[[ MAIN ]]--
